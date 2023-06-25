f0fa807cdc91704cf78cfff089f63f5db6151a77
Shreya Satyakam <89099439+Shreyasatyakam@users.noreply.github.com>
2023-06-26 01:10:42 +0530

    Update Big Countries.sql

--- Big Countries.sql
+++ Big Countries.sql
@@ -1,1 +1,2 @@
-select name,population,area from World where area>3000000 or population>25000000 order by name;
+
+select name,population,area from World where area>=3000000 or population>=25000000 ;
